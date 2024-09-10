a6117b37ddc9f26413ee42e27eac90d175f5b947
kartikey <kartikvaghasiya4477@gmail.com>
2024-09-10 19:14:03 +0530

    fourth

--- Backend/trekquest/home/urls.py
+++ Backend/trekquest/home/urls.py
@@ -3,6 +3,7 @@ from home import views
 from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = 'home'
 
@@ -11,5 +12,5 @@ urlpatterns = [
     path('customer-login/', views.reslogin, name='reslogin'), 
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
